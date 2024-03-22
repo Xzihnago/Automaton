c07039e6a438c2ffa65817021588114c4664a280
Xzihnago <xzihnago@gmail.com>
2024-03-22 14:42:22 +0800

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,14 @@
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked, {
+  rules: {
+    "@typescript-eslint/restrict-template-expressions": [
+      "error",
+      {
+        allowNumber: true,
+      },
+    ],
+  },
   languageOptions: {
     parserOptions: {
       project: true,
