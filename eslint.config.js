b0f2e5beee2130c9d814ed1c2fd7dcf2c0878262
Xzihnago <xzihnago@gmail.com>
2024-03-22 18:54:47 +0800

    Add eslint prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,25 @@
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-export default tseslint.config(...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked, {
-  rules: {
-    "@typescript-eslint/restrict-template-expressions": [
-      "error",
-      {
-        allowNumber: true,
+export default tseslint.config(
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  eslintConfigPrettier,
+  {
+    rules: {
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        {
+          allowNumber: true,
+        },
+      ],
+    },
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
       },
-    ],
-  },
-  languageOptions: {
-    parserOptions: {
-      project: true,
-      tsconfigRootDir: import.meta.dirname,
     },
   },
-});
+);
