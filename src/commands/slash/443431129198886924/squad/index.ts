6f2576fafedf0ebc3c1e96e41ceb104acd783e1a
Xzihnago <xzihnago@gmail.com>
2024-05-01 23:59:00 +0800

    Fix duplicate callback execution on message component event

--- src/commands/slash/443431129198886924/squad/index.ts
+++ src/commands/slash/443431129198886924/squad/index.ts
@@ -1,11 +1,9 @@
 import initialize from "./initialize";
 import chatInputCommand from "./chatInputCommand";
-import messageComponent from "./messageComponent";
 
 const squad: ISlashCommand<"cached"> = {
   initialize,
   chatInputCommand,
-  messageComponent,
 };
 
 export default squad;
