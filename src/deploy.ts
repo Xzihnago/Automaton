b30a6ee45e30677b39eb7ece21c390b15fdc5f66
Xzihnago <xzihnago@gmail.com>
2024-05-02 00:05:12 +0800

    Refactor deploy.ts

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,75 +1,61 @@
-import "dotenv/config";
+import { inspect } from "util";
 import {
   REST,
   Routes,
   type APIUser,
-  type SlashCommandBuilder,
-  type SlashCommandSubcommandsOnlyBuilder,
+  type SharedNameAndDescription,
 } from "discord.js";
+import "@/extensions";
+import "@/process";
+import builders from "@/commands/slash-builders";
 
-import "extensions";
-import builders from "commands/slash-builders";
-
-type TBuilders = Record<
-  string,
-  | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">
-  | SlashCommandSubcommandsOnlyBuilder
->;
-
-const putApplicationCommands = async (clientId: string, builder: TBuilders) => {
-  const timeStart = Date.now();
-  const route = Routes.applicationCommands(clientId);
-  const body = Object.values(builder);
-
-  logger.info(
-    `[Global] Put application commands ${body.map((v) => v.name).inspect()}`,
-  );
-  try {
-    await rest.put(route, { body });
-    logger.info(`[Global] Done in ${Date.now() - timeStart}ms`);
-  } catch (error) {
-    logger.error(`[Global] Error\n${(error as Error).stack}`);
-  }
-};
-
-const putApplicationGuildCommands = async (
+const putApplicationCommands = async (
+  body: SharedNameAndDescription[],
   clientId: string,
-  guildId: string,
-  builder: TBuilders,
+  guildId?: string,
 ) => {
   const timeStart = Date.now();
-  const route = Routes.applicationGuildCommands(clientId, guildId);
-  const body = Object.values(builder);
+  const scope = guildId ?? "global";
+
+  const route = guildId
+    ? Routes.applicationGuildCommands(clientId, guildId)
+    : Routes.applicationCommands(clientId);
 
   logger.info(
-    `[${guildId}] Put application guild commands ${body.map((v) => v.name).inspect()}`,
+    `[${scope}] Put application commands ${body.map((v) => v.name).inspect()}`,
   );
-  try {
-    await rest.put(route, { body });
-    logger.info(`[${guildId}] Done in ${Date.now() - timeStart}ms`);
-  } catch (error) {
-    logger.error(`[${guildId}] Error\n${(error as Error).stack}`);
-  }
+
+  await rest
+    .put(route, { body })
+    .then(() => {
+      logger.info(`[${scope}] Done in ${Date.now() - timeStart}ms`);
+    })
+    .catch((error: unknown) => {
+      logger.error(`[${scope}] Error\n${inspect(error)}`);
+    });
 };
 
-logger.info("[Deploy] Create REST interface");
+logger.info("[Deploy] Create REST");
 const rest = new REST().setToken(process.env.DISCORD_TOKEN ?? "");
 
 const user = (await rest.get(Routes.user())) as APIUser;
-logger.info(`[Deploy] Get application client(${user.id})`);
+logger.info(`[Deploy] Get Client(${user.id})`);
 
 logger.info("[Deploy] Begin");
 const timeStart = Date.now();
 
-await putApplicationCommands(user.id, builders.global);
-await putApplicationGuildCommands(user.id, "558806955380965386", {
-  ...builders.$558806955380965386,
-  ...builders.$443431129198886924,
-});
-await putApplicationGuildCommands(
-  user.id,
-  "443431129198886924",
-  builders.$443431129198886924,
-);
+await Promise.all([
+  putApplicationCommands(builders.global, user.id),
+  putApplicationCommands(
+    [...builders.$558806955380965386, ...builders.$443431129198886924],
+    user.id,
+    "558806955380965386",
+  ),
+  putApplicationCommands(
+    builders.$443431129198886924,
+    user.id,
+    "443431129198886924",
+  ),
+]);
 
 logger.info(`[Deploy] End in ${Date.now() - timeStart}ms`);
